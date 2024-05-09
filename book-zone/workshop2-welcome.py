a30d99225d7df8310ed3ef7a2880a9e173ae3f5b
Your Name <panwasa.bbey@gmail.com>
2024-05-09 23:23:24 +0700

    Complete

---
+++ book-zone/workshop2-welcome.py
@@ -0,0 +1,11 @@
+import os
+os.system('cls')
+
+print('Welcome to')
+print('')
+print('PPPPPP               TT     HH')
+print('PP   PP   YY   YY   TTTT    HH         OOOO    NN NNN')
+print('PPPPPP     YY YY     TT     HHHHHH    OO  OO   NNN  NN')
+print('PP          YYY      TT     HH   HH   OO  OO   NN   NN')
+print('PP          YY        TTT   HH   HH    OOOO    NN   NN')
+print('            Y')
