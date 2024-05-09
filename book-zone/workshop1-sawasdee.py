076684da35363be448d2f1d119f9a9893e743820
Your Name <panwasa.bbey@gmail.com>
2024-05-09 23:09:44 +0700

    Update 9/5/2024

---
+++ book-zone/workshop1-sawasdee.py
@@ -0,0 +1,6 @@
+import os
+os.system('cls')  #คำสั่ง system('cls') คือการลบพื้นที่แสดงผลให้ว่าง และให้ Cursor เม้าส์อยู่ที่บรรทัดแรก
+
+print('Welcome to my Python')
+name=input('Enter your name :')
+print('Hello',name,end='\n\n')
