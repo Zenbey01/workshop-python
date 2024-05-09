f010e3836738b47b88931adeca82a65287b6defd
Your Name <panwasa.bbey@gmail.com>
2024-05-09 23:41:41 +0700

    Complete 9/5/2024

---
+++ book-zone/workshop3-my_info.py
@@ -0,0 +1,18 @@
+import os
+os.system('cls')
+
+print('Please enter info')
+print('---------------------------------------')
+name=input('Enter your name : ')
+address=input('Enter your address : ')
+phone=input('Enter your phone number : ')
+emaill=input('Enter your E-mail : ')
+
+print('')
+
+print('This my info')
+print('---------------------------------------')
+print('My name is',name ,end='\n\n')
+print('Im from',address ,end='\n\n')
+print('My phone number',phone ,end='\n\n')
+print('Contact :',emaill ,end='\n\n')
